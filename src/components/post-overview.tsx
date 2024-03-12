f336a41537721b05ccf86fa6da9d36e217eff688
Gary50613 <50613gary@gmail.com>
2024-03-12 15:00:50 +0800

    add avatar

--- src/components/post-overview.tsx
+++ src/components/post-overview.tsx
@@ -1,4 +1,5 @@
 import { type Post } from "@/app/(auth)/posts/action";
+import { Avatar } from "@/components/avatar";
 import Link from "next/link";
 import {
   Card,
@@ -12,9 +13,16 @@ export const PostOverview = ({ post }: { post: Post }) => (
   <Link href={`/posts/${post.number}`} className="h-full">
     <Card className="flex flex-col h-full hover:bg-secondary/50 transition-colors">
       <CardHeader className="flex-grow">
-        <p className="text-secondary-foreground/75 text-sm">
-          #{post.number} • @{post.user?.login}
-        </p>
+        <div className="flex gap-2 items-center">
+          <Avatar
+            className="w-4 h-4"
+            src={post.user?.avatar_url}
+            alt={post.user?.login ?? "User avatar"}
+          />
+          <span className="text-secondary-foreground/75 text-sm">
+            #{post.number} • @{post.user?.login}
+          </span>
+        </div>
         <CardTitle className="text-lg md:text-xl h-full line-clamp-2 break-all">
           {post.title}
         </CardTitle>
