082cc1462b1263d6fe135a90a37eec6933db6ca1
Gary50613 <50613gary@gmail.com>
2024-03-14 16:44:06 +0800

    prevent re-render on posts

--- src/app/(auth)/posts/page.tsx
+++ src/app/(auth)/posts/page.tsx
@@ -1,23 +1,15 @@
 "use client";
 
-import { listPosts } from "@/app/(auth)/posts/action";
-import { type Post } from "@/common/github";
-import { PostOverview } from "@/components/post-overview";
+import { PostsLoader } from "@/app/(auth)/posts/posts-loader";
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
 import { env } from "@/env";
-import { useInfiniteData } from "@/hooks/use-infinite-data";
-import { Loader2, Plus } from "lucide-react";
+import { Plus } from "lucide-react";
 import Link from "next/link";
 
 export const runtime = "edge";
 
 export default function Page() {
-  const { ref, components, isLoading, hasMore } = useInfiniteData<Post>({
-    loader: (index) => listPosts(index),
-    render: (post) => <PostOverview key={post.number} post={post} />,
-  });
-
   return (
     <main className="space-y-4 mt-4">
       <div className="flex justify-between items-center">
@@ -41,14 +33,7 @@ export default function Page() {
         .
       </p>
       <Separator />
-      <div className="mx-auto space-y-4 w-fit">{components}</div>
-      <div ref={ref} />
-      {isLoading && (
-        <div className="flex h-16 justify-center items-center">
-          <Loader2 className="animate-spin mr-2" /> Loading posts...
-        </div>
-      )}
-      {!hasMore && !isLoading && <p className="text-center">No more posts</p>}
+      <PostsLoader />
     </main>
   );
 }
---
+++ src/app/(auth)/posts/posts-loader.tsx
@@ -0,0 +1,25 @@
+import { listPosts } from "@/app/(auth)/posts/action";
+import type { Post } from "@/common/github";
+import { PostOverview } from "@/components/post-overview";
+import { useInfiniteData } from "@/hooks/use-infinite-data";
+import { Loader2 } from "lucide-react";
+
+export const PostsLoader = () => {
+  const { ref, components, isLoading, hasMore } = useInfiniteData<Post>({
+    loader: (index) => listPosts(index),
+    render: (post) => <PostOverview key={post.number} post={post} />,
+  });
+
+  return (
+    <>
+      <div className="mx-auto space-y-4 w-fit">{components}</div>
+      <div ref={ref} />
+      {isLoading && (
+        <div className="flex h-16 justify-center items-center">
+          <Loader2 className="animate-spin mr-2" /> Loading posts...
+        </div>
+      )}
+      {!hasMore && !isLoading && <p className="text-center">No more posts</p>}
+    </>
+  );
+};
