ba1d752b18a11c3669bbba5eac2c48b85eeaa8c9
Gary50613 <50613gary@gmail.com>
2024-03-14 19:02:42 +0800

    update destructive color

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -9,14 +9,15 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        default:
+          "bg-primary text-primary-foreground hover:bg-primary/90 !no-underline",
         destructive:
-          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90 !no-underline",
         outline:
-          "border border-input bg-background hover:bg-accent hover:text-accent-foreground",
+          "border border-input bg-background hover:bg-accent hover:text-accent-foreground !no-underline",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
-        ghost: "hover:bg-accent hover:text-accent-foreground",
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80 !no-underline",
+        ghost: "hover:bg-accent hover:text-accent-foreground !no-underline",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
