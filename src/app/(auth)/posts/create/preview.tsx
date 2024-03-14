471a65d5bab9e42f5c702c448a9e4d35075242f2
Gary50613 <50613gary@gmail.com>
2024-03-14 17:29:56 +0800

    make preview margin auto

--- src/app/(auth)/posts/create/preview.tsx
+++ src/app/(auth)/posts/create/preview.tsx
@@ -23,7 +23,7 @@ export const Preview = () => {
         <AccordionTrigger>Markdown Rendered Post Preview</AccordionTrigger>
         <AccordionContent>
           {preview && (
-            <div className="prose dark:prose-invert lg:prose-xl break-words">
+            <div className="prose dark:prose-invert lg:prose-xl break-words mx-auto">
               <MDX>{`# ${title}\n${body}`}</MDX>
             </div>
           )}
