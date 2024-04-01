4316760505e3b0e54c23abb75f65110b26706a3a
Gary50613 <50613gary@gmail.com>
2024-04-02 01:04:07 +0800

    fix required scope

--- src/app/api/auth/config.ts
+++ src/app/api/auth/config.ts
@@ -2,7 +2,7 @@ import { env } from "@/env";
 
 export const loginUrl = new URL("https://github.com/login/oauth/authorize");
 
-export const scopes = ["read:user"];
+export const scopes = ["read:user", "public_repo"];
 export const scope = scopes.join(" ");
 
 loginUrl.searchParams.set("client_id", env.GITHUB_CLIENT_ID);
--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,4 +1,5 @@
 import { expect, test } from "@playwright/test";
+import { faker } from "@faker-js/faker";
 
 test("auth", async ({ page }) => {
   await page.goto("/");
@@ -10,3 +11,36 @@ test("auth", async ({ page }) => {
   await page.waitForLoadState("networkidle");
   await expect(page).toHaveURL(/\/posts/);
 });
+
+test("create post", async ({ page }) => {
+  await page.goto("/posts");
+  await page.waitForLoadState("networkidle");
+
+  await page.click("text=Create");
+
+  await expect(page).toHaveURL(/\/posts\/create/);
+
+  const { title, body } = randomPost();
+
+  await page.fill('input[name="title"]', title);
+  await page.fill('textarea[name="body"]', body);
+
+  await page.click("text=Submit");
+
+  await page.waitForLoadState("networkidle");
+
+  await expect(page).toHaveURL(/\/posts\/[0-9]+/, {
+    timeout: 10_000,
+  });
+  await expect(page).toHaveTitle(title);
+});
+
+function randomPost() {
+  return {
+    title: `Test post by ${faker.person.fullName()} in ${faker.location.city()}`,
+    body: faker.lorem.words({
+      min: 30,
+      max: 100,
+    }),
+  };
+}
