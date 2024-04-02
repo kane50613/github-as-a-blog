eb1af7902a4b3a4ff614973619662eed30f32039
Gary50613 <50613gary@gmail.com>
2024-04-02 12:15:52 +0800

    split into serial tests

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,7 +1,14 @@
-import { expect, test } from "@playwright/test";
+import { expect, type Page, test } from "@playwright/test";
 import { faker } from "@faker-js/faker";
 
-test("auth", async ({ page }) => {
+test.describe.configure({ mode: "serial" });
+
+let page: Page;
+
+test.beforeAll(async ({ browser }) => (page = await browser.newPage()));
+test.afterAll(async () => await page.close());
+
+test("auth", async () => {
   await page.goto("/");
 
   await expect(page).toHaveTitle("GaaB: Github as a Blog");
@@ -12,16 +19,13 @@ test("auth", async ({ page }) => {
   await expect(page).toHaveURL(/\/posts/);
 });
 
-test("create/edit post", async ({ page }) => {
-  await page.goto("/posts");
-  await page.waitForLoadState("networkidle");
+const { title, body } = randomPost();
 
+test("create post", async () => {
   await page.click("text=Create");
   await page.waitForLoadState("networkidle");
 
   await expect(page).toHaveURL(/\/posts\/create/);
-
-  const { title, body } = randomPost();
 
   await page.fill('input[name="title"]', title);
   await page.fill('textarea[name="body"]', body);
@@ -31,7 +35,9 @@ test("create/edit post", async ({ page }) => {
 
   await expect(page).toHaveURL(/\/posts\/[0-9]+/);
   await expect(page).toHaveTitle(title);
+});
 
+test("edit post", async () => {
   await page.click("text=Edit");
   await page.waitForLoadState("networkidle");
 
@@ -48,6 +54,17 @@ test("create/edit post", async ({ page }) => {
 
   await expect(page).toHaveURL(/\/posts\/[0-9]+/);
   await expect(page).toHaveTitle(newTitle);
+});
+
+test("delete post", async () => {
+  await page.click("text=Delete");
+  await page.waitForLoadState("networkidle");
+
+  await expect(page).toHaveURL(/\/posts\/[0-9]+\/delete/);
+  await page.click("text=Yes");
+
+  await page.waitForLoadState("networkidle");
+  await expect(page).toHaveURL(/\/posts/);
 });
 
 function randomPost() {
