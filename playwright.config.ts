eb10b92e210772572e0b8f9653176bf493b5506e
Gary50613 <50613gary@gmail.com>
2024-04-02 12:27:53 +0800

    increase retry count

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,9 +15,9 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  retries: 0,
+  retries: 1,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  // workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
