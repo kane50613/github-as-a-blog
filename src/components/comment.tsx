59867982024e64a9269e7bc8ddd63c15df3ba43f
Gary50613 <50613gary@gmail.com>
2024-04-06 15:28:58 +0800

    fix missing github comment type

--- src/components/comment.tsx
+++ src/components/comment.tsx
@@ -1,17 +1,21 @@
-import { type GithubComment } from "@/common/github";
 import { Author } from "@/components/author";
 import { MDX } from "@/components/mdx";
 import { memo } from "react";
+import { type ListIssueComments } from "@/actions/list-issue-comments";
 
-export const Comment = memo(({ comment }: { comment: GithubComment }) => (
-  <div className="flex flex-col gap-2">
-    <div className="flex justify-between items-center">
-      {comment.user && <Author user={comment.user} date={comment.updated_at} />}
+export const Comment = memo(
+  ({ comment }: { comment: ListIssueComments[number] }) => (
+    <div className="flex flex-col gap-2">
+      <div className="flex justify-between items-center">
+        {comment.user && (
+          <Author user={comment.user} date={comment.updated_at} />
+        )}
+      </div>
+      <div className="prose dark:prose-invert">
+        <MDX>{comment.body}</MDX>
+      </div>
     </div>
-    <div className="prose dark:prose-invert">
-      <MDX>{comment.body}</MDX>
-    </div>
-  </div>
-));
+  ),
+);
 
 Comment.displayName = "Comment";
