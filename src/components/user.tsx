d5d5321d18340b60a51c2831a4f2d3031d3a2fdd
Kane Wang <kane@yeecord.com>
2024-04-16 01:16:06 +0800

    use login icon to prevent layout shift

--- src/components/user.tsx
+++ src/components/user.tsx
@@ -4,14 +4,25 @@ import { Avatar } from "@/components/avatar";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { useUser } from "@/hooks/use-user";
+import { LogIn } from "lucide-react";
 
 export const User = () => {
-  const { data: user } = useUser();
+  const { data: user, isLoading } = useUser();
 
+  if (isLoading) return <Avatar className="w-8 h-8" alt="User" />;
+
+  // login button to match the size of the avatar, prevent layout shift
   if (!user)
     return (
-      <Button asChild size="sm" variant="secondary">
-        <a href="/api/auth">Login</a>
+      <Button
+        asChild
+        size="sm"
+        variant="ghost"
+        className="w-8 h-8 p-0 rounded-full"
+      >
+        <a href="/api/auth">
+          <LogIn className="w-5" />
+        </a>
       </Button>
     );
 
