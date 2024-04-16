2453147db111b3d0f0fb4c4b467a676950975346
Kane Wang <kane@yeecord.com>
2024-04-16 09:01:21 +0800

    fix layout shifting for image

--- src/components/mdx.tsx
+++ src/components/mdx.tsx
@@ -1,37 +1,48 @@
-import { createElement } from "react";
+import { createElement, type JSX, type ReactNode } from "react";
 import Markdown from "markdown-to-jsx";
+
+type Components = Partial<{
+  [Key in keyof JSX.IntrinsicElements]: (
+    props: JSX.IntrinsicElements[Key],
+    children: ReactNode,
+  ) => JSX.Element;
+}>;
+
+const components: Components = {
+  img: (props) => (
+    <span className="aspect-video w-full flex items-center justify-center">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        {...props}
+        className="rounded-md w-full h-full object-contain"
+        loading="lazy"
+        decoding="async"
+        alt={"alt" in props && typeof props.alt === "string" ? props.alt : ""}
+        key={null}
+      />
+    </span>
+  ),
+  a: (props, children) => (
+    <a
+      {...props}
+      className="foreground underline break-words"
+      rel="nofollow"
+      key={null}
+    >
+      {children}
+    </a>
+  ),
+};
 
 export const MDX = ({ children }: { children: string }) => (
   <Markdown
     options={{
       createElement(type, props, children) {
-        if (type === "img")
-          return (
-            // since we can't get the width and height of the image, we won't be using the `next/image` component.
-            // eslint-disable-next-line @next/next/no-img-element
-            <img
-              {...props}
-              className="rounded-md"
-              loading="lazy"
-              decoding="async"
-              alt={
-                "alt" in props && typeof props.alt === "string" ? props.alt : ""
-              }
-              key={null}
-            />
-          );
+        if (typeof type === "string") {
+          const renderer = components[type as keyof typeof components];
 
-        if (type === "a")
-          return (
-            <a
-              {...props}
-              className="foreground underline break-words"
-              rel="nofollow"
-              key={null}
-            >
-              {children}
-            </a>
-          );
+          if (renderer) return renderer(props, children);
+        }
 
         return createElement(type, props, children);
       },
